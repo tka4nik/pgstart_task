a9aa06120cc86e1b853bb86e22b1c3d5410d4b36
tka4nik <tka4nik@gmail.com>
2024-05-16 01:31:27 +0300

    Refactored connection function, polished code

--- README.md
+++ README.md
@@ -1,1 +1,32 @@
-# pgstart_task
+# SSH Postgres Installer
+
+This script automates the installation and configuration of PostgreSQL on a remote server via SSH.
+
+## Usage
+
+To use this script, execute the following command:
+
+```bash
+python install_postgres.py <host>
+```
+
+Replace <host> with the hostname or IP address of the target server.
+
+## Requirements
+
+    Python 3.x
+    paramiko library (`pip install paramiko`)
+
+## Implementation Details
+
+    - The script provides two ways to connect to the provided host - via an SSH key, or via username/password if the former attempt is unsuccesfull.
+    - `sudo` support:
+        - You **cannot** simply use `sudo su - username` in an `exec_command` before the command that needs root access, because each call to `exec_command` runs in its own subshell, as such the changes will not propagate back to the main session.
+        - I tried to overcome this issue by writing a simple function-wrapper around `exec_command`, which (along side proper exceptions handling) checks if the command requires sudo perms, and if it does, handles admin password input if needed.
+    - Script is designed to work on Debian-based distros and on the latest postgresql version (usage of `apt` and the hardcoded path to the .conf file).
+        - It is possible to adapt the solution to check both for an existing package manager by checking the distro first (debian/SuSE/redhat/gentoo/alpine/arch/..) in the folder `/etc/os-release
+
+## Author
+
+Tkachenko Nikita, Moscow, Higher School of Economics: 3-rd year, Applied Mathematics, GPA: 7.3
+My CV: [link](https://drive.google.com/file/d/1-f2QIuVbLOe0MWIgp7u80G45PKlaL0cL/view?usp=sharing)
--- main.py
+++ main.py
@@ -1,45 +1,35 @@
 import sys
 
 import paramiko
-import psycopg2
 
 
 def connect(host: str):
     client = paramiko.SSHClient()
     try:
-        # k = paramiko.RSAKey.from_private_key_file("id_rsa")
-        # print(k)
-        k = None
-        if k is not None:
-            client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-            client.connect(hostname=host, port=22)
-            return client
-        else:
-            print("SSH key was not found, input credentials:")
-            username = input("Username: ")
-            password = input("Password: ")
-            client.connect(hostname=host, username=username, password=password)
-            return client
+        key = input("SSH key path:")
+        pkey = paramiko.RSAKey.from_private_key_file(key)
+        client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+        client.connect(hostname=host, port=22, pkey=pkey)
+        return client
+    except Exception as e:
+        print("RSA file error: ", e)
+
+    try:
+        print("SSH key was not found, input credentials:")
+        username = input("Username: ")
+        password = input("Password: ")
+        client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+        client.connect(hostname=host, username=username, password=password)
+        return client
     except Exception as e:
         print("Error:", e)
         return None
 
 
-def execute_command_ssh(ssh_client, command, sudo=False):
-    """
-    Execute a command on a remote server using SSH.
-
-    Args:
-        ssh_client (paramiko.SSHClient): SSH client object connected to the remote server.
-        command (str): Command to execute.
-        sudo (bool, optional): Whether to execute the command with sudo. Defaults to False.
-
-    Returns:
-        tuple: A tuple containing the command exit status, the command output, and any error message.
-    """
+def execute_command_ssh(client: paramiko.SSHClient, command, sudo=False):
     try:
         if sudo:
-            stdin, stdout, stderr = connection.exec_command("sudo -S %s" % command, get_pty=True)
+            stdin, stdout, stderr = client.exec_command("sudo -S %s" % command, get_pty=True)
             if stdout.channel.closed is False:
                 stdin.write('%s\n' % input("Admin password: "))
                 stdin.flush()
@@ -47,7 +37,7 @@ def execute_command_ssh(ssh_client, command, sudo=False):
             output = stdout.read().decode().strip()
             error = stderr.read().decode().strip()
         else:
-            stdin, stdout, stderr = ssh_client.exec_command(command, get_pty=True)
+            stdin, stdout, stderr = client.exec_command(command, get_pty=True)
 
             exit_status = stdout.channel.recv_exit_status()
             output = stdout.read().decode().strip()
@@ -55,25 +45,32 @@ def execute_command_ssh(ssh_client, command, sudo=False):
 
         return exit_status, output, error
     except paramiko.SSHException as e:
-        return None, None, f"SSHException: {e}"
+        print(f"SSHException: {e}")
+        return None, None, "SSHException: {e}"
     except Exception as e:
+        print(f"Error: {e}")
         return None, None, f"Error: {e}"
 
 
 def install_and_configure_postgresql(client: paramiko.SSHClient):
     execute_command_ssh(client, "apt-get update", True)
     execute_command_ssh(client, "apt-get -y install postgresql postgresql-contrib", True)
-    execute_command_ssh(client, "sed -i 's/#listen_addresses = 'localhost'/listen_addresses = '*'/' /etc/postgresql/13/main/postgresql.conf",True)  # Assuming ubuntu, that's the default location of the config
+    execute_command_ssh(client,
+                        "sed -i 's/#listen_addresses = 'localhost'/listen_addresses = '*'/' /etc/postgresql/13/main/postgresql.conf",
+                        True)  # Assuming ubuntu, that's the default location of the config; version number is hardcoded
     execute_command_ssh(client, "echo 'host all all 0.0.0.0/0 md5' >> /etc/postgresql/13/main/pg_hba.conf", True)
     execute_command_ssh(client, "systemctl restart postgresql", True)
+    print("Installation complete!")
 
 
 def check_installation(client: paramiko.SSHClient):
     try:
         stdin, stdout, stderr = execute_command_ssh(client, "psql -c 'SELECT 1;'")
-        print(stdout)
+        print("Testing installation, running 'SELECT 1;': ", stdout)
+        return True
     except Exception as e:
-        return None, None, f"Error: {e}"
+        print("Error:", e)
+        return False
 
 
 if __name__ == "__main__":
@@ -83,12 +80,12 @@ if __name__ == "__main__":
         sys.exit(1)
 
     host = sys.argv[1]
-    connection = connect(host)
-    if connection is None:
+    client = connect(host)
+    if client is None:
         print("Invalid credentials.")
         sys.exit(2)
 
-    install_and_configure_postgresql(connection)
-    if check_installation(connection):
+    install_and_configure_postgresql(client)
+    if check_installation(client):
+        print("Success!")
         sys.exit(0)
-    sys.exit(1)
